90c18067910caa92ac054ec63e1471afb7932474
maxcravo <maxcravo25@gmail.com>
2022-02-23 18:05:31 -0300

    adicionando nova página de task

--- projeto-to-do-list/central_task/code.js
+++ projeto-to-do-list/central_task/code.js
@@ -16,6 +16,18 @@ function completed_task() {
   }
 }
 
+function redirect() {
+
+  let element = document.querySelectorAll('li');
+
+  for (let k = 0; k < element.length; k++) {
+    element[k].addEventListener("click", function() {
+      window.location.replace("http://127.0.0.1:5500/projeto-to-do-list/task_page/Task_page.html")
+
+    })
+  }
+}
+
 function new_task() {
 
   let new_task = document.getElementById("add").value;
@@ -29,7 +41,6 @@ function new_task() {
   let complete_task = document.createElement("input");
   complete_task.type = "checkbox";
   complete_task.id = "complete_tasks";
-
 
   let new_task_item = document.createTextNode(new_task);
   new_task_div.appendChild(new_element);
@@ -53,17 +64,12 @@ function new_task() {
   // pegar cada task e atribuir o valor vindo do tamanho da mesma ao seu id
   // podendo depois acessar esse id individualmente para cada task
 
-
-
   completed_task();
+  redirect();
   return new_task_item
 }
 /* função que vai remover o indicador de lista caso não seja criado uma
 task */
-
-
-
-
 
 function validate(e) {
   if (event.key === "Enter") {
